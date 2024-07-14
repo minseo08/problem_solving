8bb563e051ef6d51398ba4ab7e983f5b9bf8fb2e
minseo08 <superider00@gmail.com>
2024-07-14 14:45:00 +0900

    modify

--- string/25501.cpp
+++ string/25501.cpp
@@ -8,7 +8,6 @@ int recursion(const char *s, int l, int r){
     else if(s[l] != s[r]) return 0;
     else return recursion(s, l + 1, r - 1);
 }
-
 int isPalindrome(const char *s){
     return recursion(s, 0, strlen(s) - 1);
 }
