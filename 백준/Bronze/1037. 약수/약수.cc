bae4e90d80da24f422e188ffcea951b1a2d40298
kms <70687598+minseo08@users.noreply.github.com>
2025-02-10 14:29:12 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.cc
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -5,20 +5,14 @@ using namespace std;
 vector<int> vec;
 
 int main(){
-    int cnt, res;
-    cin >> cnt;
-    int y;
-    while(cin >> y){
+    int n, y;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> y;
         vec.push_back(y);
     }
-    int s = vec.size();
     sort(vec.begin(), vec.end());
-    if(s == 1){
-        res = vec[0] * vec[0];
-    }
-    else{
-        res = vec[0] * vec[s - 1];
-    }
+    int res = vec[0] * vec[n - 1];
     cout << res;
     return 0;
 }
