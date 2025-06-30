71533c2458123c0859d96366b4ef60d896c3132a
kms <70687598+minseo08@users.noreply.github.com>
2025-06-30 17:58:35 +0900

    modify 호텔.cc

--- 백준/Gold/1106. 호텔/호텔.cc
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -1,35 +1,38 @@
 #include <iostream>
 #include <algorithm>
-#define MAX 1100
 #define INF 987654321
+#define MAX 2001
 using namespace std;
 pair<int, int> arr[21];
-int dp[21][1110];
+int dp[21][MAX];
 
 int main(){
-    for(int i = 0; i < 21; i++){
-        for(int j = 0; j < 1110; j++)
-            dp[i][j] = INF;
-    }
     int c, n;
     cin >> c >> n;
     for(int i = 1; i <= n; i++){
-        cin >> arr[i].first >> arr[i].second;
+        cin >> arr[i].first >> arr[i].second; // 비용, 고객
+    }
+    for(int i = 0; i < 21; i++){
+        for(int j = 1; j < MAX; j++)
+            dp[i][j] = INF;
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j < MAX; j++){
+            if(j >= arr[i].second){
+                dp[i][j] = min(dp[i][j - arr[i].second] + arr[i].first, dp[i - 1][j]);
+            }
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
     }
     int res = INF;
     for(int i = 1; i <= n; i++){
-        dp[i][0] = 0;
-        for(int j = 1; j <= MAX; j++){
-            if(j >= arr[i].second){
-                dp[i][j] = dp[i][j - arr[i].second] + arr[i].first;
-            }
-            dp[i][j] = min(dp[i][j], dp[i - 1][j]);
-        }
-        for(int j = c; j <= MAX; j++){
+        for(int j = c; j < MAX; j++)
             res = min(res, dp[i][j]);
-        }
+        //     cout << dp[i][j] << " ";
+        // cout << "\n";
     }
     cout << res;
+    
     return 0;
 }
-
