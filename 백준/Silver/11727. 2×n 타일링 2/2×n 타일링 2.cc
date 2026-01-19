f97e6cb99477db2dbbfe68515d81a6e17eaae769
kms <70687598+minseo08@users.noreply.github.com>
2026-01-19 18:09:05 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int dp[n];
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int i = 3; i <= n; i++){
+        dp[i] = ((dp[i - 2] * 2 + dp[i - 1]) % 10007);
+    }
+    cout << dp[n];
+    return 0;
+}
